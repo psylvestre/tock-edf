7ecfe5573f3e296118818fb98e98d773181b6314
Philippe Sylvestre <news4me@free.fr>
2020-11-13 10:13:33 +0100

    Test Connector AlloMedia

--- edf-bot/src/main/kotlin/edf/GreetingsStory.kt
+++ edf-bot/src/main/kotlin/edf/GreetingsStory.kt
@@ -29,16 +29,48 @@ import ai.tock.bot.connector.slack.slackButton
 import ai.tock.bot.connector.slack.slackMessage
 import ai.tock.bot.connector.slack.withSlack
 import ai.tock.bot.definition.story
+import ai.tock.bot.open.data.story.destination
+import ai.tock.bot.open.data.story.location
+import ai.tock.bot.open.data.story.origin
+import mu.KotlinLogging
 import opendata.sampleButton
 import opendata.sampleMessage
 import opendata.withSample
+import org.litote.kmongo.json
+import org.litote.kmongo.util.idValue
 
 /**
  * The greetings handler.
  */
+private val logger = KotlinLogging.logger {}
+
 val greetings = story("hello") {
     //cleanup state
     resetDialogState()
+
+    logger.info { "Info Action ${this.action}" }
+    logger.info { "Info BotDefinition ${this.botDefinition}" }
+    logger.info { "Info ConnectorData ${this.connectorData}" }
+    logger.info { "Info Dialog ${this.dialog}" }
+    logger.info { "Info Entities ${this.entities}" }
+    logger.info { "Info UserTimeLine ${this.userTimeline}" }
+    logger.info { "Info UserText ${this.userText}" }
+    logger.info { "Info Story ${this.story}" }
+    logger.info { "Info Step ${this.step}" }
+    logger.info { "Info UserPreferences ${this.userPreferences}" }
+    logger.info { "Info Intent ${this.intent}" }
+    logger.info { "Info ApplicationId ${this.applicationId}" }
+    logger.info { "Info BotId ${this.botId}" }
+    logger.info { "Info ContextId ${this.contextId}" }
+    logger.info { "Info UserId ${this.userId}" }
+    logger.info { "Info StepName ${this.stepName}" }
+    logger.info { "Info UserLocale ${this.userLocale}" }
+    logger.info { "Info Destination ${this.destination}" }
+    logger.info { "Info Location ${this.location}" }
+    logger.info { "Info Origin ${this.origin}" }
+    logger.info { "Info IdValue ${this.idValue}" }
+    logger.info { "Info Json ${this.json}" }
+
 
     end("Oui que puis-je faire pour vous")
 }
