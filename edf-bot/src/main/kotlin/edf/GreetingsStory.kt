6222eb9f5a4169ac6fe7b0bf996eedf13b596b64
Philippe Sylvestre <news4me@free.fr>
2020-11-17 19:43:42 +0100

    Test Tock UserTimeLine

--- edf-bot/src/main/kotlin/edf/GreetingsStory.kt
+++ edf-bot/src/main/kotlin/edf/GreetingsStory.kt
@@ -27,6 +27,7 @@ import ai.tock.bot.open.data.story.destination
 import ai.tock.bot.open.data.story.location
 import ai.tock.bot.open.data.story.origin
 import ai.tock.shared.jackson.mapper
+import edf.Utilities
 import edf.allomedia.connector.EDFAlloMediaMessage
 import edf.allomedia.connector.withEDFAlloMedia
 import edf.allomedia.response.EDFStoryResponse
@@ -43,65 +44,33 @@ val greetings = story("hello") {
     //cleanup state
     resetDialogState()
 
-    logger.info { "Info Action ${this.action}" }
-    logger.info { "Info BotDefinition ${this.botDefinition}" }
-    logger.info { "Info ConnectorData ${this.connectorData}" }
-    logger.info { "Info Dialog ${this.dialog}" }
-    logger.info { "Info Entities ${this.entities}" }
-    logger.info { "Info UserTimeLine ${this.userTimeline}" }
-    logger.info { "Info UserTimeLine ${this.userTimeline.dialogs.size}" }
-    logger.info { "Info UserTimeLine ${this.userTimeline.dialogs[0].id}" }
-    logger.info { "Info UserTimeLine ${this.userTimeline.dialogs[0].stories.size}" }
-    logger.info { "Info UserTimeLine ${this.userTimeline.dialogs[0].stories[0].starterIntent}" }
-    logger.info { "Info UserTimeLine ${this.userTimeline.dialogs[0].stories[0].definition}" }
-    logger.info { "Info Json Dialogs ${mapper.writeValueAsString(this.userTimeline.dialogs[0])}"}
-    logger.info { "Info Json Dialogs Stories ${mapper.writeValueAsString(this.userTimeline.dialogs[0].stories[0])}"}
-    logger.info { "Info UserText ${this.userText}" }
-    logger.info { "Info Story ${this.story}" }
-    logger.info { "Info Step ${this.step}" }
-    logger.info { "Info UserPreferences ${this.userPreferences}" }
-    logger.info { "Info Intent ${this.intent}" }
-    logger.info { "Info ApplicationId ${this.applicationId}" }
-    logger.info { "Info BotId ${this.botId}" }
-    logger.info { "Info ContextId ${this.contextId}" }
-    logger.info { "Info UserId ${this.userId}" }
-    logger.info { "Info StepName ${this.stepName}" }
-    logger.info { "Info UserLocale ${this.userLocale}" }
-    logger.info { "Info Destination ${this.destination}" }
-    logger.info { "Info Location ${this.location}" }
-    logger.info { "Info Origin ${this.origin}" }
-    logger.info { "Info IdValue ${this.idValue}" }
-    //logger.info { "Info Json ${this.json}" }
+    Utilities.logData(this)
 
     val response: EDFStoryResponse =
         EDFStoryResponse(
-            //this.action,
             null,
-            //this.botDefinition,
             null,
-            //this.connectorData,
             null,
-            this.dialog,
-            this.entities,
-            this.userTimeline,
-            this.userText,
-            this.story,
-            //this.step,
-            //this.userPreferences,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
             null,
             this.intent,
             this.applicationId,
             this.botId,
             this.contextId,
             this.userId,
-            //this.stepName,
+            null,
             this.userLocale,
-            //this.idValue,
-            //null,
+            null,
+            null,
             "Oui que puis-je faire pour vous",
-            this.userTimeline.dialogs[0].stories[0]
+            null
         )
 
-    withEDFAlloMedia(EDFAlloMediaMessage(true))
+//    withEDFAlloMedia(EDFAlloMediaMessage(true))
     end(mapper.writeValueAsString(response))
 }
